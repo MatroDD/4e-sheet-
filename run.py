a547090ba4fb666ed1967ad768b2085d2ddc4270
Inquisitor Okabe † <53707439+InquisitorOkabe@users.noreply.github.com>
2021-10-14 04:54:24 +0300

    Добавил комменты

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
+# --- Импорты ---
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
 from ui import Ui_MainWindow
 
+# --- Инициализация графического окна ---
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 
+# --- Создания базовых классов ---
 class God:
     def __init__(self):
-        self.ui = ui
+        self.ui = ui # Файл интерфейса
 
+        # Словарь с костылём для подсчёта модификатора статов
         self.help = {
             1:-5,
             2:-4,
@@ -44,6 +48,7 @@ class God:
             30:10,
             31:10,
         }
+        # Словарь с подсказками формулы рассчёта статов
         self.toolTip = {
             'str':lambda: self.ui.st_str_all.setToolTip(f'<html><head/><body><p><span style=" font-weight:600;">Итоговый стат:</span> {self.ui.st_str_origin.text()} + {self.ui.st_str_misc.text()} = {self.ui.st_str_all.text()}<br/><span style=" font-weight:600;">Формула:</span> &lt;БазовыйСтат&gt; + &lt;ВременныйМод&gt; = &lt;ИтоговыйСтат&gt;</p></body></html>'),
             'dex':lambda: self.ui.st_dex_all.setToolTip(f'<html><head/><body><p><span style=" font-weight:600;">Итоговый стат:</span> {self.ui.st_dex_origin.text()} + {self.ui.st_dex_misc.text()} = {self.ui.st_dex_all.text()}<br/><span style=" font-weight:600;">Формула:</span> &lt;БазовыйСтат&gt; + &lt;ВременныйМод&gt; = &lt;ИтоговыйСтат&gt;</p></body></html>'),
@@ -53,6 +58,8 @@ class God:
             'cha':lambda: self.ui.st_cha_all.setToolTip(f'<html><head/><body><p><span style=" font-weight:600;">Итоговый стат:</span> {self.ui.st_cha_origin.text()} + {self.ui.st_cha_misc.text()} = {self.ui.st_cha_all.text()}<br/><span style=" font-weight:600;">Формула:</span> &lt;БазовыйСтат&gt; + &lt;ВременныйМод&gt; = &lt;ИтоговыйСтат&gt;</p></body></html>'),
             }
         # --- Connect ---
+        # Добавляем к полям ввода статов и модификаторов событие "если изменён текст"
+        # Тут же привязываем это событие к функции
         self.ui.st_str_origin.textChanged.connect(self.st_Str_Calc)
         self.ui.st_str_misc.textChanged.connect(self.st_Str_Calc)
         self.ui.st_dex_origin.textChanged.connect(self.st_Dex_Calc)
@@ -66,12 +73,19 @@ class God:
         self.ui.st_cha_origin.textChanged.connect(self.st_Cha_Calc)
         self.ui.st_cha_misc.textChanged.connect(self.st_Cha_Calc)
 
+    # Функции подсчёта статов
     def st_Str_Calc(self):
+        # Вход в блок исключений, чтобы не класть ошибки если вловим хуйню
         try:
+            # Установить текст общего значения стата
             self.ui.st_str_all.setText(
+                # складываем начальный стат и временный модификатор
+                # приводим их к числовому типу и обратно к строке
                 str(int(self.ui.st_str_origin.text()) + int(self.ui.st_str_misc.text()))
                 )
+            # Устанавливаем модификатор через словарь с костылём
             self.ui.st_str_mod.setText(str(self.help[int(self.ui.st_str_all.text())]))
+            # Вызываем из словаря лямбду с генерацией описания
             self.toolTip['str']()
         except BaseException:
             pass
@@ -130,7 +144,11 @@ class God:
         except BaseException:
             pass
         pass
+# Созадём основной класс для взаимосвязи приложения
 god = God()
 
+# Показыаем окно и запускаем цикл.
+# После sys.exit(app.exec_()) никакой код писать нельзя
+# он выполнится ТОЛЬКО когда мы ЗАКРОЕМ окно
 MainWindow.show()
 sys.exit(app.exec_())
