cf91f948fab4ba6e00246a2786eb4aaf2604e3f4
Inquisitor Okabe † <53707439+InquisitorOkabe@users.noreply.github.com>
2021-10-25 15:27:05 +0300

    Добавлена переключаемая автономия

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 # --- Импорты ---
 import sys, os
 
-# Включим потом. Не трогать.
+# Чтобы сделать программу автономной - уберите сивмол # в начале строк, между -Началом- и -Концом-.
+# Так же убедитесь - что перед строкой нет пробелов.
+# Чтобы выключить - верните символ # обратно.
+# --- Начало ---
 # scriptdir, script = os.path.split(__file__)
 # installdir = scriptdir  # for compatibility with commands
 # pkgdir = os.path.join(scriptdir, 'Python310/Lib/site-packages')
 # sys.path.insert(0, pkgdir)
+# --- Конец ---
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ui import Ui_MainWindow
---
+++ Автономный запуск (Читайте readme).bat
@@ -0,0 +1,1 @@
+Python310\Pythonw.exe run.py
